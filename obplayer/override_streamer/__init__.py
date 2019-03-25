b11da86c534e3235333910bf11f386e936ab96b0
Vincent Maggard <maggardalertnetwork@gmail.com>
2019-03-25 16:59:00 -0400

    Added support for overrides returning to normal programming.'

--- obplayer/override_streamer/__init__.py
+++ obplayer/override_streamer/__init__.py
@@ -32,50 +32,42 @@ class Streamer(obplayer.ObThread):
     def __init__(self):
         obplayer.ObThread.__init__(self, 'Obremote_station_override')
         #self.ctrl = obplayer.Player.create_controller('remote station override', priority=99, allow_requeue=False, default_play_mode='overlap')
-        self.ctrl = obplayer.Player.create_controller('remote station override', priority=99, allow_requeue=False)
+        self.ctrl = obplayer.Player.create_controller('remote station override', priority=99)
         self.ffmpeg = None
         self.stream_active = False
         self.running = True
         self.daemon = True
-
+        self.self_sending_override = False
 
     def start_override(self, url):
-        print('Starting Override...')
-        #self.ffmpeg = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-re', '-i', url, '-c:a', 'pcm_s16le', '-f', 'rtp', 'rtp://localhost:5000'], stdout=subprocess.PIPE)
+        #print('Starting Override...')
         self.ffmpeg = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-re', '-i', url, '-c:a', 'libopus', '-f', 'rtp', 'rtp://239.255.255.255:5000'], stdout=subprocess.PIPE)
-        #import os
-        #os.system(' '.join(['ffmpeg', '-loglevel', 'quiet', '-re', '-i', url, '-c:a', 'pcm_s16le', '-f', 'rtp', 'rtp://239.255.255.255:5000']))
-        #print(self.ffmpeg.poll())
-        #self.ctrl.add_request(media_type='rtp_2', start_time=time.time() + 4, duration=153)
-        #self.ctrl.add_request(media_type='rtp', start_time=time.time() + 4, duration=153, uri="rtp://239.255.255.255:5000")
-        self.ctrl.add_request(media_type='rtp_2', start_time=time.time() + 4, duration=153, uri="rtp://239.255.255.255:5000")
+        self.ctrl.add_request(media_type='remote_audio', start_time=time.time() + 4, duration=31536000, uri="rtp://239.255.255.255:5000")
 
     def stop_override(self):
         if self.ffmpeg != None:
+            #print('Ending Override...')
             self.ffmpeg.kill()
+            self.ffmpeg = None
+        self.ctrl.stop_requests()
+        self.ctrl.add_request(media_type='break', duration=1)
 
     def check_stream_status(self, stats_url, stream_url):
         req = requests.get(stats_url)
-        #print(req.content.decode())
         if req.status_code == 200:
             try:
                 data = json.loads(req.content.decode())
                 try:
                     if data['icestats'].get('source') != None:
                         if data['icestats']['source']['listenurl'] == stream_url.replace('127.0.0.1', 'localhost'):
-                            print('True')
                             return True
-                    print('False')
                     return False
                 except Exception as e:
                     for stream in data['icestats']['source']:
                         if stream_url == stream['listenurl'].replace('127.0.0.1', 'localhost'):
-                            print('TEST:' + stream['listenurl'])
                             return True
             except Exception as e:
                 #print(e)
-                #print('TEST')
-                #print(req.content)
                 return False
         else:
             return False
@@ -86,20 +78,21 @@ class Streamer(obplayer.ObThread):
         mountpoint = obplayer.Config.setting('station_override_server_mountpoint')
         stream_url = 'http://{0}:{1}/{2}'.format(ip, port, mountpoint)
         while self.running:
-            if self.check_stream_status('http://{0}:{1}/status-json.xsl'.format(ip, port), stream_url) and self.stream_active == False:
-                print('Override stream Found...')
-                self.stream_active = True
-                self.start_override(stream_url)
-            time.sleep(6)
-        if self.stream_active:
-            self.stop_override()
+            if self.check_stream_status('http://{0}:{1}/status-json.xsl'.format(ip, port), stream_url):
+                #print('Override stream Found...')
+                if self.stream_active == False:
+                    self.stream_active = True
+                    self.start_override(stream_url)
+                time.sleep(6)
+            else:
+                if self.stream_active:
+                    self.stop_override()
+                self.stream_active = False
 
 
     def try_run(self):
         try:
             self.background()
-            #print('Testing override...')
-            #self.ctrl.add_request(media_type='rtp_2', start_time=time.time(), duration=153)
         except Exception as e:
             print(e)
             raise
@@ -107,32 +100,9 @@ class Streamer(obplayer.ObThread):
     def stop(self):
         self.running = False
 
-# def linein_request(self, present_time, media_class):
-#     self.add_request(media_type='linein', duration=31536000)        # duration = 1 year (ie. indefinitely)
-#
-# def background(self):
-#     stream_active = False
-#     ip = obplayer.Config.setting('station_override_server_ip')
-#     port = obplayer.Config.setting('station_override_server_port')
-#     mountpoint = obplayer.Config.setting('station_override_server_mountpoint')
-#     stream_url = 'http://{0}:{1}/{2}'.format(ip, port, mountpoint)
-#     while self.running:
-#         req = requests.get(stream_url)
-#         if req.status_code == 200:
-#             stream_active = True
-
-
-
-
 def init():
     streamer = Streamer()
     streamer.start()
-    #print('TEST 4')
-    #ctrl = obplayer.Player.create_controller('linein', priority=10, allow_requeue=False)
-    #def linein_request(self, present_time, media_class):
-    #     self.add_request(media_type='linein', duration=31536000)        # duration = 1 year (ie. indefinitely)
-    #ctrl = obplayer.Player.create_controller('linein', priority=99, allow_requeue=False)
-    #ctrl.set_request_callback(linein_request)
 
 def quit():
     pass
